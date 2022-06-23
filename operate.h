7812b4017c9fe6b2024a01d004710b9a5519efae
linhkhnguyen98 <107893768+linhkhnguyen98@users.noreply.github.com>
2022-06-22 22:02:27 -0700

    Add files via upload

---
+++ operate.h
@@ -0,0 +1,10 @@
+#ifndef _OPERATE_H
+#define _OPERATE_H
+#include "instruction.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/// do an operation specified by the instrution pointer
+extern int operate(int, instr_t*);
+extern int check_stack(unsigned int, int);
+#endif
